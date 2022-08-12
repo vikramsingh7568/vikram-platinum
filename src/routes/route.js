9b5ea1aa8295aa55ba09a1c926ffa893be5f9b61
vikramsingh7568 <110588921+vikramsingh7568@users.noreply.github.com>
2022-08-12 17:22:08 +0530

    all the code on git hub

--- src/routes/route.js
+++ src/routes/route.js
@@ -7,7 +7,7 @@ const { Welcome } = require('../logger/logger');
 const Second =require('../introduction/util/helper');//cretinng require for second task
 const Third = require('../introduction/validator/formatter')//creating require for third task
 const router = express.Router();
-
+//let  app = express.Router();
 
 router.get('/test-me', function (req, res) {
     console.log('My batch is', abc.name)
@@ -130,5 +130,379 @@ router.get('/get-/films/:indexNumber',function(req, res){ //student detail api h
 })
 
 
+
+// adding this comment for no reason**************************************************************
+
+//this is the first problem of 9-8-22
+router.get('/missing1', function(req, res){
+    let arr =[1,2,3,4,5,6,7,8,9,10,11,13,14,15,16]
+    
+    function missingNumber(arr){
+    for(let i=0; i<arr.length; i++){
+        if(i+1 != arr[i]){
+           return arr[i]-1
+        }
+     }    
+    }
+   let   missing_number = missingNumber(arr)
+    // console.log(result)
+    //res.send(result.toString())
+     res.send('this number is not found ='+missing_number)
+})
+
+
+router.get("/sol2", function (req, res) {
+
+    //logic : sum of n consecutive numbers is [ n * (first + last) / 2  ]..so get sum of all numbers in array. now take sum of n consecutive numbers.. n would be length+1 as 1 number is missing
+ //2nd problem solution  9-8-2022
+    let arr= [33, 34, 35, 37, 38]
+     let len= arr.length
+     let total = 0;
+     for (var i in arr) {
+         total += arr[i];
+     }
+     let firstDigit= arr[0]
+     let lastDigit= arr.pop()
+     let consecutiveSum= (len + 1) * (firstDigit+ lastDigit ) / 2
+     let missingNumber= consecutiveSum - total
+  
+    res.send(   'missing number = '+ missingNumber    );
+   });
+
+   //10 -8 - 2022 lecrure on node ke notes idhar he ************************************
+
+router.get('/this/testing', function (req,res){
+    res.send('this is testing again wer are testing')
+})
+
+
+
+router.get('/this/testing2', function (req,res){
+    res.send('this is testinng 2 wow maja aa gaya sab apne aap hota')
+})
+
+
+router.get('/this/testing3', function (req,res){
+    res.send({ a: 6, b: 34, msg: "maja aa gaya wow ye to badi chhi chij he  "  })
+})
+
+//post api use krte taki data safe or secure rahe 
+
+
+router.post('/this-post', function (requ,res){
+    let id = requ.body.user //req.body me value aati he 
+    let password = requ.body.password
+    console.log("this is id      =", id)
+    console.log("this is password = ",password)
+    console.log(requ.body) //user id pass print krega
+    res.send('code successully chal gaya he ')
+})
+
+//take input in post request and add it in the aaray and return the aaray
+router.post('/test-pos-4', function (req,res){
+     let arr = [12,"vikram"] //hamara array he ye 
+     let input = req.body.element //eleement me value lenge udharr se postman se
+     arr.push(input) //input me aai value ko hamare array me push kar diya
+    res.send({ msg: arr, status: true}) //returning data in object form 
+    // msg is key arr is value status is key true is value
+    // res.send({arr})
+})
+
+//assignment solution is here 10-8-22
+
+let players =[
+    {
+        "name":"manish",
+        "dob" :"1/1/1995",
+        "gender":"male",
+        "city":"jalandhar",
+        "sports":[
+            "swimming"
+        ],
+    },
+     
+    {
+        "name":"gopal",
+        "dob" :"1/09/1995",
+         "gender": "male",
+         "city" :"delhi",
+         "sports":[
+            "soccer"
+         ],
+    },
+      
+     {
+         "name":"lokesh",
+         "dob" :"1/1/1990",
+         "gender":"male",
+          "city":"mumbai",
+          "sports":[
+            "soccer"
+          ],
+          
+        },
+        
+    ]
+
+    router.post('/players', function (req,res){
+      //  console.log(req.body)
+      let name1   = req.body.name;
+      let dob1    = req.body.dob
+      let gender1 = req.body.gender
+      let city1   = req.body.city
+      let sports1  = req.body.sports
+    //  console.log(name1)
+   //console.log(players[0].name)
+    for(let i=0 ; i<players.length; i++){
+        if(players[i].name ==name1){
+           return res.send("this data already exist")
+        }
+    }
+         players.push(name1)
+         players.push(dob1)
+         players.push(gender1)
+         players.push(city1)
+         players.push(sports1)  
+        res.send({data : players, status : true})
+    
+    })
+
+
+// router.post('/players', function(req, res){
+
+//     let newPlayer = req.body
+//     let newPlayersName = newPlayer.name
+//    // console.log(newPlayer)
+//     let isNameRepeated = false
+
+    // let player condition
+    //  for(let i=0; i<players.length; i++){
+    //     if(players[i].name == newPlayersName){
+    //         isNameRepeated = true;
+    //         break;
+    //     }
+    //  }
+
+//        //undeifned is same as  false 
+//             if(isNameRepeated == true){
+//                 res.send(' ye aadmi pele se he idhar ')
+//             }else
+//             {
+//                 players.push(newPlayer)
+//                 res.send(players)
+//             }
+            
+//     })
+
+    //wer are searching for parmas 
+  router.get('/parm:number',function(req, res){
+    let number = req.params.number //this print the value which you push in teh url
+    let value1 = req.params //this will prit that entered value is object
+    res.send("this is working"+number)
+  })
+
+
+//next 2nd question solution*******************************************************
+
+let players1 =[
+    {
+        "name":"manish",
+        "bookingNumber": 1,
+        "dob" :"1/1/1995",
+        "gender":"male",
+        "city":"jalandhar",
+        "sports":[
+            "swimming"   ],
+       
+    "sportId": "",
+    // "centerId : '',
+   "type": "private",
+   "slot": '16286598000000',
+   "bookedOn":'31/08/2021',
+   "bookedFor":'01/09/2021',
+    },
+     
+    
+     
+    {
+        "name":"gopal",
+        "bookingNumber": 2,
+        "dob" :"1/09/1995",
+         "gender": "male",
+         "city" :"delhi",
+         "sports":[
+            "soccer"
+         ],
+        
+         "sportId": "",
+         // "centerId : '',
+        "type": "private",
+        "slot": '16286598000000',
+        "bookedOn":'31/08/2021',
+        "bookedFor":'01/09/2021',
+    },
+      
+     {
+         "name":"lokesh",
+         "bookingNumber":3,
+         "dob" :"1/1/1990",
+         "gender":"male",
+          "city":"mumbai",
+          "sports":[
+            "soccer"
+          ],
+         
+          "sportId": "",
+          // "centerId : '',
+         "type": "private",
+         "slot": '16286598000000',
+         "bookedOn":'31/08/2021',
+         "bookedFor":'01/09/2021',
+          
+        },
+        
+    ]
+
+    let play = [
+        {
+          "name" :"vikram",
+           "bookingNumber":"1",
+           "dob" :"1/1/1995",
+           "gender":"male",
+           "city":"jalandhar",
+           "sports":[
+               "swimming"   ],
+          
+       "sportId": "",
+    //    "centerId : '',
+      "type": "private",
+      "slot": '16286598000000',
+      "bookedOn":'31/08/2021',
+      "bookedFor":'01/09/2021',
+        },
+          {
+            "name":"manish",
+            "bookingNumber":"2",
+            "dob" :"1/09/1995",
+            "gender": "male",
+            "city" :"delhi",
+            "sports":[
+               "soccer"
+            ],
+           
+            "sportId": "",
+            // "centerId : '',
+           "type": "private",
+           "slot": '16286598000000',
+           "bookedOn":'31/08/2021',
+           "bookedFor":'01/09/2021',
+          },
+          {
+            "name":"rahul",
+            "bookingNumber":"3",
+            "dob" :"1/1/1990",
+            "gender":"male",
+             "city":"mumbai",
+             "sports":[
+               "soccer"
+             ],
+            
+             "sportId": "",
+             // "centerId : '',
+            "type": "private",
+            "slot": '16286598000000',
+            "bookedOn":'31/08/2021',
+            "bookedFor":'01/09/2021',
+          }
+         
+      ]
+      
+
+    router.post('/playerss', function(req, res){
+
+        // let Playername = req.params.Playername
+        // let Playerid   = req.params.Playerid
+
+         
+         let Playername = req.body.Playername
+         let Playerid   = req.body.Playerid
+      for(let i=0; i<play.length; i++){
+           if(play[i].name == Playername &&  play[i].bookingNumber == Playerid){
+            console.log("this person is already exist")
+            return res.send("this person is already exist")
+            
+           }
+          
+      }
+           for(let j=0 ; j<play.length; j++){
+            if(play[j].name == Playername && play[j].bookingNumber != Playerid){
+                console.log("welcome your id is generated")
+                return  res.send("wlcome your id is generated ")     
+            }
+      }
+    
+      for(let k=0 ; k<play.length; k++){
+        if(play[k].name != Playername  ){
+            console.log("sorry this name is not available in our player list")
+            return  res.send("sorry this name is not available in our player list ")     
+        }
+  }
+   
+})
+//Assignment 3rd problem*******************************************************
+
+let persons = [
+    {
+      name : "PK",
+      age : 10,
+      votingstatus : false
+    },
+    {
+        name : "Sk",
+        age : 20,
+        votingstatus : false
+    },
+    {
+        name : "AA",
+        age : 70,
+        votingstatus : false
+    },
+    {
+        name : "SC",
+        age : 5,
+        votingstatus : false
+    },
+    {
+        name : "HQ",
+        age : 40,
+        votingstatus : false
+    }
+]
+router.post("/persons", function(req,res){
+     let InputAge=req.query.Age 
+    let person=[]
+     let flag =false
+   for(let i=0; i<persons.length; i++){
+      if(persons[i].age >=InputAge && InputAge >=18){
+        persons[i].votingstatus=true
+         person.push(persons[i])
+         flag =true
+      }
+   }
+      if(flag==true){
+     res.send({vall :"Eligible for voting", data : person, votingstatus :true})
+    }
+
+     for(let i=0; i<persons.length; i++){
+        if(persons[i].age <=InputAge && InputAge <=18){
+          persons[i].votingstatus=false
+           person.push(persons[i])
+        }
+     }   
+        if(flag==false){
+       res.send({va:"not eligible for voting", data : person, votingstatus :flag})
+    }
+
+})
+
 module.exports = router;
-// adding this comment for no reason
