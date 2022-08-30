525b9c220608dbfec76a1f08a244ceedffccb29f
vikramsingh7568 <110588921+vikramsingh7568@users.noreply.github.com>
2022-08-30 10:51:15 +0530

    29-8-22-Assignment-JWt-Authentication

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,12 +1,64 @@
-const authenticate = function(req, req, next) {
-    //check the token in request header
-    //validate this token
+const jwt = require('jsonwebtoken')
+const userModel = require("../models/userModel")
+const mongoose = require('mongoose')
 
-    next()
+
+
+const authenticate = async function(req, res) {
+
+  let userName = req.body.emailId;
+  let user = await userModel.findOne({emailId : userName})
+  let token = jwt.sign(
+    {
+      userId: user._id.toString(),
+      batch: "Plutonium",
+      organisation: "Function-up",
+    },
+    "functionup-Plutonium-key"
+  );
+  res.setHeader("x-auth-token", token);
+  res.send({ status: true, data: token });
+};
+    
+// authorise ******************************************************************************************************88
+const authorise = function(req, res, next ) {
+  let token = req.headers["x-Auth-token"];
+  if (!token) token = req.headers["x-auth-token"];
+  if (!token) return res.send({ status: false, msg: "token must be present" });
+
+  let decodedToken = jwt.verify(token, "functionup-Plutonium-key",(err , decode) => {
+    if(err){
+      return res.send("you have entered incorrect token or. incorrect length of token")
+    } (decode == true)
+        next()
+    
+  });
 }
 
+const authorise2 = function(req, res, next) {
+  let token = req.headers["x-Auth-token"];
+  if (!token) token = req.headers["x-auth-token"];
+  if (!token) return res.send({ status: false, msg: "token must be present" });
 
-const authorise = function(req, res, next) {
-    // comapre the logged in user's id and the id in request
+  let decodedToken = jwt.verify(token, "functionup-Plutonium-key")
+let userLoggedIn = decodedToken.userId
+let userToBeModified = req.params.userId
+  
+  let isValid = mongoose.Types.ObjectId.isValid(userToBeModified)
+  
+ if (isValid === false){
+    return  res.send("length of the id is less then 24 digit")
+   }
+  else if (!decodedToken){
+    return res.send({ status: false, msg: "token is invalid" });
+  } 
+  else  if(userToBeModified != userLoggedIn) {
+  return res.send({status: false, msg: 'User logged is not allowed to modify the requested users data'})
+  }else {
     next()
+}  
 }
+
+module.exports.authenticate =authenticate
+module.exports.authorise =authorise
+module.exports.authorise2 = authorise2
--- src/routes/route.js
+++ src/routes/route.js
@@ -1,20 +1,20 @@
 const express = require('express');
 const router = express.Router();
 const userController= require("../controllers/userController")
-
+const middleware = require("../middleware/auth")
 router.get("/test-me", function (req, res) {
     res.send("My first ever api!")
 })
 
-router.post("/users", userController.createUser)
+router.post("/createusers", userController.createUser)
 
-router.post("/login", userController.loginUser)
+router.post("/loginuser", userController.loginUser , middleware.authenticate )
 
 //The userId is sent by front end
-router.get("/users/:userId", userController.getUserData)
-router.post("/users/:userId/posts", userController.postMessage)
+router.get("/getusers/:userId",middleware.authorise, middleware.authorise2, userController.getUserData)
+router.post("/postusers/:userId/posts",middleware.authorise,middleware.authorise2, userController.postMessage)
 
-router.put("/users/:userId", userController.updateUser)
-router.delete('/users/:userId', userController.deleteUser)
+router.put("/updateusers/:userId", middleware.authorise ,middleware.authorise2, userController.updateUser)
+ router.delete('/deleteusers/:userId',middleware.authorise ,middleware.authorise2, userController.deleteUser)
 
 module.exports = router;
